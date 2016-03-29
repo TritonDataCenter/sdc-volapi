b93af57db58221c8ed3fee1d83c9508653858bf1
Julien Gilli <julien.gilli@joyent.com>
2016-03-29 16:42:44 -0700

    Add boot scripts for the vapi zone

---
+++ boot/configure.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+# -*- mode: shell-script; fill-column: 80; -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Copyright (c) 2016, Joyent, Inc.
+#
+
+export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+set -o xtrace
+
+echo "Updating SMF manifest"
+$(/opt/local/bin/gsed -i"" -e "s/@@PREFIX@@/\/opt\/smartdc\/vapi/g" /opt/smartdc/vapi/smf/manifests/vmapi.xml)
+
+echo "Importing vapi.xml"
+/usr/sbin/svccfg import /opt/smartdc/vapi/smf/manifests/vapi.xml
+
+exit 0
